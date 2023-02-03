9463e06ddf72d1fc422e4d491fdc997159f4df73
LeonidHome <defenser2205@gmail.com>
2023-02-03 22:22:58 +0300

    Task 3.2

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,7 @@ public class Main {
 
     }
     public static void task3 () {
-        System.out.println("Задача 2");
+        System.out.println("Задача 3");
         String firstName = " Семён";
         String middleName = " Семёнович";
         String lastName = "Иванов";
