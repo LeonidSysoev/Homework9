73e3804d6eead2c52f1148c83537d7ec3428bf24
LeonidHome <defenser2205@gmail.com>
2023-02-03 19:41:04 +0300

    Task 1-2

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        String firstName = " Ivan";
+        String middleName = " Ivanovich";
+        String lastName = "Ivanov";
+        String fullName;
+        fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+        String firstName = " Ivan";
+        String middleName = " Ivanovich";
+        String lastName = "Ivanov";
+        String fullName;
+        fullName = lastName + firstName + middleName;
+        String fullNameUp = fullName.toUpperCase();
+        System.out.println("ФИО сотрудника — " + fullNameUp);
+
+    }
+}
